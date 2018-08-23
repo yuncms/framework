1fe7b8377803c91fa165d03399c35cbcf1d32119
XuTongle <xutongle@gmail.com>
2018-08-23 18:37:42 +0800

    update cors

--- src/rest/Controller.php
+++ src/rest/Controller.php
@@ -54,6 +54,24 @@ class Controller extends \yii\rest\Controller
             'class' => CompositeAuth::class,
             'authMethods' => $this->authMethods,
         ];
+        $behaviors['cors'] = [
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::className(),
+                'cors' => [
+                    // restrict access to
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['POST', 'PUT', 'GET'],
+                    // Allow only POST and PUT methods
+                    'Access-Control-Request-Headers' => ['*'],
+                    // Allow only headers 'X-Wsse'
+                    'Access-Control-Allow-Credentials' => true,
+                    // Allow OPTIONS caching
+                    'Access-Control-Max-Age' => 3600,
+                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+                ],
+            ],
+        ];
         return $behaviors;
     }
 
